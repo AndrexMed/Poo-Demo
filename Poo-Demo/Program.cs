6f4b54155d0f8a9ab88f65ad6b716ea408116453
GiovanniAlzate <galzate@cesde.edu.co>
2023-12-18 14:49:03 -0500

    Agregar archivos de proyecto.

---
+++ Poo-Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Apuntador apuntadorPlatzi = new Apuntador();
+Apuntador apuntadorCesde = new Apuntador();
+
+apuntadorPlatzi.Color = "Blue";
+
+class Apuntador
+{
+    public string Color;
+    public double Largo;
+    public short NumeroBotones;
+    public bool TieneStickers;
+}
